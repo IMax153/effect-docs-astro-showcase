245c37ea81e9a4a287df22b047c37f8946cd33ae
Giulio Canti <giulio.canti@gmail.com>
2024-09-26 09:19:57 +0200

    test snippets

---
+++ src/snippets/why-effect/divide-effect.ts
@@ -0,0 +1,6 @@
+import { Effect } from "effect"
+
+const divide = (a: number, b: number): Effect.Effect<number, Error, never> =>
+  b === 0
+    ? Effect.fail(new Error("Cannot divide by zero"))
+    : Effect.succeed(a / b)
