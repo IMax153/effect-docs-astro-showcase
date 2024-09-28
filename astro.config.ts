67d68640e01b483d6a82688ee875896de748be85
Maxwell Brown <maxwellbrown1990@gmail.com>
2024-09-28 11:28:06 -0400

    fix site

--- astro.config.ts
+++ astro.config.ts
@@ -9,7 +9,7 @@ import codeOutputPlugin from "./src/plugins/code-output"
 /* https://docs.netlify.com/configure-builds/environment-variables/#read-only-variables */
 const NETLIFY_PREVIEW_SITE = process.env.CONTEXT !== "production" && process.env.DEPLOY_PRIME_URL;
 
-const site = NETLIFY_PREVIEW_SITE || undefined /* TODO: "https//effect.website" */
+const site = NETLIFY_PREVIEW_SITE || "https//effect.website"
 
 const rehypeMermaidOptions: RehypeMermaidOptions = {
   strategy: "img-svg",
@@ -21,7 +21,7 @@ const remarkCodeImportOptions: Parameters<typeof remarkCodeImport>[0] = {
 }
 
 export default defineConfig({
-  site: "http://localhost:4321",
+  site,
   markdown: {
     rehypePlugins: [[rehypeMermaid, rehypeMermaidOptions]],
     remarkPlugins: [[remarkCodeImport as any, remarkCodeImportOptions]],
