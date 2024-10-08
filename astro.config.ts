8ae435b2198f356416707b334909be6dd5bcb14f
Giulio Canti <giulio.canti@gmail.com>
2024-10-08 16:21:40 +0200

    add missing page

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config"
 import { h } from "hastscript"
 import rehypeMermaid, { type RehypeMermaidOptions } from "rehype-mermaid"
-import rehypeAutolinkHeadings, { type Options as RehypeAutolinkHeadingsOptions } from "rehype-autolink-headings"
+import rehypeAutolinkHeadings, {
+  type Options as RehypeAutolinkHeadingsOptions
+} from "rehype-autolink-headings"
 import starlightBlog from "starlight-blog"
 import starlightLinksValidator from "starlight-links-validator"
 import { rehypeHeadingIds } from "@astrojs/markdown-remark"
@@ -29,34 +31,36 @@ const rehypeMermaidOptions: RehypeMermaidOptions = {
 
 const rehypeAutolinkHeadingsOptions: RehypeAutolinkHeadingsOptions = {
   properties: {
-    class: "anchor-link",
+    class: "anchor-link"
   },
   behavior: "after",
   group: ({ tagName }) =>
     h("div", {
       tabIndex: -1,
-      class: `autolink-heading-container level-${tagName}`,
+      class: `autolink-heading-container level-${tagName}`
     }),
-  content: () => [h(
-    "span",
-    {
-      ariaHidden: "true",
-      class: "anchor-icon",
-    },
+  content: () => [
     h(
-      "svg",
+      "span",
       {
-        width: 16,
-        height: 16,
-        viewBox: "0 0 24 24",
+        ariaHidden: "true",
+        class: "anchor-icon"
       },
-      h("path", {
-        fill: "currentcolor",
-        d: "m12.11 15.39-3.88 3.88a2.52 2.52 0 0 1-3.5 0 2.47 2.47 0 0 1 0-3.5l3.88-3.88a1 1 0 0 0-1.42-1.42l-3.88 3.89a4.48 4.48 0 0 0 6.33 6.33l3.89-3.88a1 1 0 1 0-1.42-1.42Zm8.58-12.08a4.49 4.49 0 0 0-6.33 0l-3.89 3.88a1 1 0 0 0 1.42 1.42l3.88-3.88a2.52 2.52 0 0 1 3.5 0 2.47 2.47 0 0 1 0 3.5l-3.88 3.88a1 1 0 1 0 1.42 1.42l3.88-3.89a4.49 4.49 0 0 0 0-6.33ZM8.83 15.17a1 1 0 0 0 1.1.22 1 1 0 0 0 .32-.22l4.92-4.92a1 1 0 0 0-1.42-1.42l-4.92 4.92a1 1 0 0 0 0 1.42Z",
-      }),
-    ),
-  )]
-};
+      h(
+        "svg",
+        {
+          width: 16,
+          height: 16,
+          viewBox: "0 0 24 24"
+        },
+        h("path", {
+          fill: "currentcolor",
+          d: "m12.11 15.39-3.88 3.88a2.52 2.52 0 0 1-3.5 0 2.47 2.47 0 0 1 0-3.5l3.88-3.88a1 1 0 0 0-1.42-1.42l-3.88 3.89a4.48 4.48 0 0 0 6.33 6.33l3.89-3.88a1 1 0 1 0-1.42-1.42Zm8.58-12.08a4.49 4.49 0 0 0-6.33 0l-3.89 3.88a1 1 0 0 0 1.42 1.42l3.88-3.88a2.52 2.52 0 0 1 3.5 0 2.47 2.47 0 0 1 0 3.5l-3.88 3.88a1 1 0 1 0 1.42 1.42l3.88-3.89a4.49 4.49 0 0 0 0-6.33ZM8.83 15.17a1 1 0 0 0 1.1.22 1 1 0 0 0 .32-.22l4.92-4.92a1 1 0 0 0-1.42-1.42l-4.92 4.92a1 1 0 0 0 0 1.42Z"
+        })
+      )
+    )
+  ]
+}
 
 export default defineConfig({
   site,
@@ -73,7 +77,7 @@ export default defineConfig({
   integrations: [
     tailwind({
       applyBaseStyles: false,
-      nesting: true,
+      nesting: true
     }),
     starlight({
       title: "Effect Documentation",
@@ -266,6 +270,11 @@ export default defineConfig({
           label: "Platform",
           badge: { text: "Unstable", variant: "caution" },
           autogenerate: { directory: "docs/platform" },
+          collapsed: false
+        },
+        {
+          label: "Integrations",
+          autogenerate: { directory: "docs/integrations" },
           collapsed: false
         },
         {
